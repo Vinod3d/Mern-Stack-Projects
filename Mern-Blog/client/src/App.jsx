55d89e028b72a4d4970a0348828d8a350f9433f6
Vinod Chandra <vinodchandra979@gmail.com>
2024-02-25 08:28:04 +0530

    navbar compleate

--- Mern-Blog/client/src/App.jsx
+++ Mern-Blog/client/src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div>
-        <h2 className='text-3xl'>App Component</h2>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route  path="/" element={<Home/>} />
+        <Route  path="/about" element={<About/>} />
+        <Route  path="/sign-in" element={<SignUp/>} />
+        <Route  path="/sign-up" element={<Signin/>} />
+        <Route  path="/dashboard" element={<Dashboard/>} />
+        <Route  path="/projects" element={<Projects/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
