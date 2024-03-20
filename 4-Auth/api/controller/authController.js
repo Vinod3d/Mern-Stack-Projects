cb1f9794a675013dac28d4c16979a2da3ad1b3ce
Vinod Chandra <vinodchandra979@gmail.com>
2024-03-20 21:39:08 +0530

    Auth route compleate

--- 4-Auth/api/controller/authController.js
+++ 4-Auth/api/controller/authController.js
@@ -11,7 +11,7 @@ const signup = async (req, res, next) => {
        await newUser.save();
        res.status(201).json({message: 'User created successfully'});
    } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
    }
 };
 
--- 4-Auth/api/index.js
+++ 4-Auth/api/index.js
@@ -16,6 +16,16 @@ app.use(express.json()); // middleware for parsing json
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/user', userRouter);
 
+app.use((err, req, res, next) =>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  })
+})
+
 
 const port = process.env.PORT;
 const start = async () => {
