486b7262d77fd510a8bf41c4017cac8846a01491
Vinod Chandra <vinodchandra979@gmail.com>
2024-03-23 15:36:53 +0530

    add delete user functionality

--- 4-Auth/api/controller/userController.js
+++ 4-Auth/api/controller/userController.js
@@ -41,7 +41,16 @@ const updateUser = async (req, res, next) => {
 
 
 const deleteUser = async (req, res, next) => {
-    res.send("delete User")
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, 'You can delete only your account!'));
+  }
+
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json('User has been deleted...');
+  } catch (error) {
+    next(error);
+  }
 }
 
 
--- 4-Auth/client/src/pages/Profile.jsx
+++ 4-Auth/client/src/pages/Profile.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux"
-import {getStorage, ref, getDownloadURL, uploadBytesResumable} from 'firebase/storage'
-import {app} from '../firebase'
+import { getStorage, ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase'
 import { useDispatch } from "react-redux";
-import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice";
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure } from "../redux/user/userSlice";
 
 const Profile = () => {
   const dispatch = useDispatch();
@@ -12,11 +12,11 @@ const Profile = () => {
   const [imagePercent, setImagePercent] = useState(0);
   const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState({});
-  const {currentUser, loading, error} = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [updateSuccess, setUpdateSuccess] = useState(false);
 
-  useEffect(()=>{
-    if(image){
+  useEffect(() => {
+    if (image) {
       handleFileUpload(image);
     }
   }, [image]);
@@ -29,21 +29,21 @@ const Profile = () => {
     uploadTask.on('state_changed', (snapshot) => {
       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
       setImagePercent(Math.round(progress));
-      },
-    
-      (error)=>{
+    },
+
+      (error) => {
         setImageError(true)
       },
-      ()=>{
+      () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          setFormData({...formData, profilePicture: downloadURL})
+          setFormData({ ...formData, profilePicture: downloadURL })
         })
       }
     );
   }
 
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.id] : e.target.value});
+    setFormData({ ...formData, [e.target.id]: e.target.value });
   }
 
   const handleSubmit = async (e) => {
@@ -59,7 +59,7 @@ const Profile = () => {
       });
 
       const data = await res.json();
-      if (data.success === false){
+      if (data.success === false) {
         dispatch(updateUserFailure(data));
       }
 
@@ -70,76 +70,92 @@ const Profile = () => {
     }
   }
 
+  const handleDeleteAccount = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/v1/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(deleteUserFailure(data));
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error));
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
-        <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <input 
-            type="file" 
-            ref={fileRef} 
-            hidden
-            accept='image/*'
-            onChange={(e)=> setImage(e.target.files[0])}
-          />
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept='image/*'
+          onChange={(e) => setImage(e.target.files[0])}
+        />
 
-          <img 
-            src={formData.profilePicture || currentUser.profilePicture} 
-            alt="profile" 
-            className="h-24 w-24 self-center cursor-pointer rounded-full object-cover"
-            onClick={()=> fileRef.current.click()}
-          />
+        <img
+          src={formData.profilePicture || currentUser.profilePicture}
+          alt="profile"
+          className="h-24 w-24 self-center cursor-pointer rounded-full object-cover"
+          onClick={() => fileRef.current.click()}
+        />
 
-          <p className='text-sm self-center'>
-            {imageError ? (
-              <span className="text-red-700">Error uploading Image (file size must be less than 2 MB)</span>
-            ) : imagePercent > 0 && imagePercent < 100 ? (
-              <span className="text-slate-700">{`Uploading : ${imagePercent}%`}</span>
-            ) : imagePercent === 100 ? (
-              <span className="text-green-700">Image uploaded successfully</span>
-            ) : ""}
-          </p>
+        <p className='text-sm self-center'>
+          {imageError ? (
+            <span className="text-red-700">Error uploading Image (file size must be less than 2 MB)</span>
+          ) : imagePercent > 0 && imagePercent < 100 ? (
+            <span className="text-slate-700">{`Uploading : ${imagePercent}%`}</span>
+          ) : imagePercent === 100 ? (
+            <span className="text-green-700">Image uploaded successfully</span>
+          ) : ""}
+        </p>
 
-          <input
-            defaultValue={currentUser.username}
-            type='text'
-            id='username'
-            placeholder='Username'
-            className='bg-slate-100 rounded-lg p-3'
-            onChange={handleChange}
-          />
-          <input
-            defaultValue={currentUser.email}
-            type='email'
-            id='email'
-            placeholder='Email'
-            className='bg-slate-100 rounded-lg p-3'
-            onChange={handleChange}
-          />
-          <input
-            type='password'
-            id='password'
-            placeholder='Password'
-            className='bg-slate-100 rounded-lg p-3'
-            onChange={handleChange}
-          />
-          <button 
+        <input
+          defaultValue={currentUser.username}
+          type='text'
+          id='username'
+          placeholder='Username'
+          className='bg-slate-100 rounded-lg p-3'
+          onChange={handleChange}
+        />
+        <input
+          defaultValue={currentUser.email}
+          type='email'
+          id='email'
+          placeholder='Email'
+          className='bg-slate-100 rounded-lg p-3'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          id='password'
+          placeholder='Password'
+          className='bg-slate-100 rounded-lg p-3'
+          onChange={handleChange}
+        />
+        <button
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-            update
-          </button>
-        </form>
+          {loading ? 'Loading...' : 'Update'}
+        </button>
+      </form>
 
-        <div className='flex justify-between mt-5'>
-          <span
-            // onClick={handleDeleteAccount}
-            className='text-red-700 cursor-pointer'
-          >
-            Delete Account
-          </span>
-          <span 
+      <div className='flex justify-between mt-5'>
+        <span
+          onClick={handleDeleteAccount}
+          className='text-red-700 cursor-pointer'
+        >
+          Delete Account
+        </span>
+        <span
           // onClick={handleSignOut}
           className='text-red-700 cursor-pointer'>
-            Sign out
-          </span>
+          Sign out
+        </span>
       </div>
 
       <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
