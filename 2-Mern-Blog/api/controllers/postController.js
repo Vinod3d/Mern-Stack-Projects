e1fd11cb5bd0d3576d45e192da04b6e2caeb7ee8
Vinod Chandra <vinodchandra979@gmail.com>
2024-04-24 17:03:16 +0530

    Add Delete Post functionality

--- 2-Mern-Blog/api/controllers/postController.js
+++ 2-Mern-Blog/api/controllers/postController.js
@@ -75,9 +75,24 @@ const getposts = async(req, res, next)=>{
 }
 
 
+const deletepost = async (req, res, next) =>{
+    if(!req.user.isAdmin || req.user.id !== req.params.userId){
+        return next(CustomError.UnauthorizedError('You are not allowed to delete this post'));
+    }
+    
+    try {
+        await  Post.findByIdAndDelete(req.params.postId);
+        res.status(StatusCodes.OK).json('The post has been deleted');
+    } catch (error) {
+        next(error.message);
+    }
+}
+
+
 
 module.exports = {
     create,
     getposts,
+    deletepost,
 
 }
--- 2-Mern-Blog/api/routes/postRoutes.js
+++ 2-Mern-Blog/api/routes/postRoutes.js
@@ -4,9 +4,11 @@ const verifyToken = require('../utils/verifyUser');
 const {
     create,
     getposts,
+    deletepost
 } =  require("../controllers/postController");
 
 router.post('/create', verifyToken, create)
 router.get('/getposts', getposts)
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
 
 module.exports = router;
